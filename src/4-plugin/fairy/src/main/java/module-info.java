b694ab7e50f56620a689204f5dea48e61857a98c
Pedro Andrez <pedro.andrez@coriant.com>
2017-10-19 16:22:43 +0100

    Added Log4j

--- src/4-plugin/fairy/src/main/java/module-info.java
+++ src/4-plugin/fairy/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module org.gradle.fairy.app {
     requires org.gradle.fairy.tale;
+    requires log4j.api;
+
     uses org.gradle.fairy.tale.Tale;
 }
--- src/4-plugin/fairy/src/main/java/org/gradle/fairy/app/StoryTeller.java
+++ src/4-plugin/fairy/src/main/java/org/gradle/fairy/app/StoryTeller.java
@@ -1,12 +1,17 @@
 package org.gradle.fairy.app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.gradle.fairy.tale.Tale;
 
 import java.util.ServiceLoader;
 
 class StoryTeller {
 
+    private static final Logger LOGGER = LogManager.getLogger(StoryTeller.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Starting the tales...");
         ServiceLoader<Tale> loader = ServiceLoader.load(Tale.class);
         if (!loader.iterator().hasNext()) {
             System.out.println("Alas, I have no tales to tell!");
@@ -16,3 +21,4 @@ class StoryTeller {
         }
     }
 }
+
